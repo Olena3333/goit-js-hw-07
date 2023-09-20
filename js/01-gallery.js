7a996a570ac81499efcbd9c0d35fddb33295cada
Olena <helensmart2323@gmail.com>
2023-09-20 15:58:16 +0200

    02task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const markUp = galleryItems.map(({ preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`   
+}).join("");
+
+ulEl.insertAdjacentHTML("beforeend", markUp);
+
+let modal = null;
+ulEl.addEventListener('click', onOpenMadal);
+function onOpenMadal(e) {
+   e.preventDefault();
+ if (e.target.nodeName !== "IMG") return;
+  modal = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="1000" >`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscapeClick);
+        document.body.style.overflow = "hidden"
+      },
+      onClose: () => {
+  document.removeEventListener('keydown', onEscapeClick);
+  document.body.style.overflow = "visible"
+}
+    }
+  )
+  modal.show(); 
+}
+
+function onEscapeClick(e) {
+  if (e.code !== "Escape") return;
+    closeModal();
+}
+
+function closeModal() {
+  modal.close()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const markUp = galleryItems.map(({ preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`   
+}).join("");
+
+ulEl.insertAdjacentHTML("beforeend", markUp);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
